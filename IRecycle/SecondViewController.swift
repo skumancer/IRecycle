aac894db66aac77e54e7265ab86d979d37c24873
Ricardo Chavarria <ricardo.chavarria@admios-sa.com>
2016-12-08 00:09:35 -0500

    Initial Commit

---
+++ IRecycle/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  IRecycle
+//
+//  Created by Ricardo Chavarria on 12/8/16.
+//  Copyright © 2016 Ricardo Chavarria. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
